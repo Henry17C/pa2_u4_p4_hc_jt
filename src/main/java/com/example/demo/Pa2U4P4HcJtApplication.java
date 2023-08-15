e7b78f79136412a1f2d3ce9af544bb92ff3fda98
Henry17C <hicoyago@uce.edu.ec>
2023-08-14 19:49:53 -0500

    taller38

---
+++ src/main/java/com/example/demo/Pa2U4P4HcJtApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U4P4HcJtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U4P4HcJtApplication.class, args);
+	}
+
+}
